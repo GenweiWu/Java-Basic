c0b1064fd02416fda27d4008921bb914fc60c623
GenweiWu <wugenwei2010@163.com>
2017-02-18 15:21:58 +0800

    ThreadLocal的使用 1、初始值的设置 2、使用线程池时的注意事项

---
+++ BasicDemo/src/com/njust/thread/MultipleThreadDemo.java
@@ -0,0 +1,62 @@
+package com.njust.thread;
+
+import java.util.UUID;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * ThreadLocal--多线程问题
+ */
+public class MultipleThreadDemo {
+    private static ThreadLocal<String> HOLDER = new ThreadLocal<>();
+
+    public static void main(String[] args) {
+        MultipleThreadDemo multipleThreadDemo = new MultipleThreadDemo();
+//        multipleThreadDemo.testWithThread();
+        multipleThreadDemo.testWithThreadPool();
+    }
+
+    /**
+     * 1.2 验证线程池下可能有问题(如果不是每个线程，每次都设置下值的话)
+     * 注意线程池下的线程出现重用时，值重复出现了！！！
+     Thread[pool-1-thread-1,5,main]-->a7becfb6-81ca-4cec-8a8f-e8a59b1c5bb3
+     Thread[pool-1-thread-2,5,main]-->4176f47f-09f3-461d-a2d9-c9b8e7acbe0c
+     Thread[pool-1-thread-2,5,main]-->4176f47f-09f3-461d-a2d9-c9b8e7acbe0c
+     */
+    private void testWithThreadPool() {
+        Task task = new Task();
+
+        Executor executor = Executors.newFixedThreadPool(2);
+        for (int i = 1; i <= 3; i++) {
+            executor.execute(task);
+        }
+    }
+
+    /**
+     * 1.1 验证多线程下使用正常
+     * <p>
+     * Thread[Thread-0,5,main]-->b664514f-3261-4096-a4ac-44449ae3d681
+     * Thread[Thread-1,5,main]-->f15ea2e2-1778-41d3-b6d1-92c44304e9dd
+     * Thread[Thread-2,5,main]-->ba6edb1c-e186-4af2-b462-08ca606b6684
+     */
+    private void testWithThread() {
+        Task task = new Task();
+        for (int i = 1; i <= 3; i++) {
+            Thread thread = new Thread(task);
+            thread.start();
+        }
+    }
+
+
+    public class Task implements Runnable {
+        @Override
+        public void run() {
+            String detail = HOLDER.get();
+            if (detail == null) {
+                detail = "" + UUID.randomUUID();
+                HOLDER.set(detail);
+            }
+            System.out.println(Thread.currentThread() + "-->" + detail);
+        }
+    }
+}
---
+++ BasicDemo/src/com/njust/thread/ThreadLocalDemo.java
@@ -0,0 +1,60 @@
+package com.njust.thread;
+
+
+import java.util.UUID;
+
+/**
+ * 1、ThreadLocal的使用
+ * 2、ThreadLocal--自定义初始值
+ */
+public class ThreadLocalDemo {
+    private static ThreadLocal<String> HOLDER = ThreadLocal.withInitial(() -> "Up!");
+
+    public static void main(String[] args) {
+        ThreadLocalDemo threadLocalDemo = new ThreadLocalDemo();
+        threadLocalDemo.testSimple();
+    }
+
+
+    /**
+     * testSimple:从下面的值可以看出初始化值为自定义内容+每个线程可以获取各自的线程的数据
+     * <p>
+     * init :Up!
+     * init :Up!
+     * init :Up!
+     * getValue: b7abbd88-7697-4dcc-b907-e3a583748f26
+     * getValue: 85258158-6828-415c-88b3-f3d4934da873
+     * getValue: 60bc0772-aaf5-4016-b970-46b39d821c41
+     */
+    private void testSimple() {
+        Task task = new Task();
+        for (int i = 1; i <= 3; i++) {
+            Thread thread = new Thread(task);
+            thread.start();
+        }
+    }
+
+
+    public class Task implements Runnable {
+        @Override
+        public void run() {
+            test01();
+            test02();
+        }
+
+        private void test01() {
+            String detail = HOLDER.get();
+            System.out.println("init :" + detail);
+
+            HOLDER.set("" + UUID.randomUUID());
+        }
+
+        private void test02() {
+            System.out.println("getValue: " + HOLDER.get());
+        }
+
+
+    }
+}
+
+
