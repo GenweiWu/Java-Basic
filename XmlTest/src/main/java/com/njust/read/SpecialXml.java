649377a42bf7c238129a760c29b38ce9e9cc5947
GenweiWu <wugenwei2010@163.com>
2017-04-24 20:57:11 +0800

    xml项目优化-引入maven

--- XmlTest/src/main/java/com/njust/read/SpecialXml.java
+++ XmlTest/src/main/java/com/njust/read/SpecialXml.java
@@ -11,13 +11,13 @@ public class SpecialXml {
 		SAXReader reader = new SAXReader();
 		Document doc = reader.read(new File("src/a.xml"));
 
-		Element book1 = (Element) doc.selectObject("//data/book1");
+		Element book1 = (Element) doc.selectSingleNode("//data/book1");
 		String name1 = book1.attributeValue("name");
 		System.out.println(name1);
 		String text1 = book1.getTextTrim();
 		System.out.println(text1);
 
-		Element book2 = (Element) doc.selectObject("//data/book2");
+		Element book2 = (Element) doc.selectSingleNode("//data/book2");
 		String text2 = book2.getTextTrim();
 		System.out.println(text2);
 	}
